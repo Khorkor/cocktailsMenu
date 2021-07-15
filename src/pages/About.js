802bf8374083bb9ead2c0ab9b5fe1232e5d9db99
Khorkor <korhankantar@gmail.com>
2021-07-15 12:23:52 +0300

    cocktailsmenu done

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab
+        minima provident itaque distinctio expedita magni error nisi asperiores
+        perspiciatis? Neque obcaecati maiores temporibus nesciunt, consectetur
+        quibusdam ad similique velit aperiam exercitationem ipsum dolor!
+        Obcaecati incidunt odio minus dolore nobis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
